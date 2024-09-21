540d39549461bf1af5cbd06dada47d1cdea4cc91
rnp2795 <death.pointer0t@icloud.com>
2024-09-21 12:24:32 +0300

    Small fix.

--- src/components/layout/game-footer.tsx
+++ src/components/layout/game-footer.tsx
@@ -1,9 +1,11 @@
 export const GameFooter = () => {
   return (
     <div className="border-t py-3 px-3 flex items-center justify-center">
-      <p>{`${import.meta.env.VITE_GAME_NAME} version ${
-        import.meta.env.VITE_GAME_VERSION
-      }`}</p>
+      <div>
+        <p>{`${import.meta.env.VITE_GAME_NAME} version ${
+          import.meta.env.VITE_GAME_VERSION
+        }`}</p>
+      </div>
     </div>
   );
 };
--- src/components/layout/game-main.tsx
+++ src/components/layout/game-main.tsx
@@ -1,8 +1,14 @@
 export const GameMain = () => {
   return (
-    <div className="py-3 px-3">
-      <div id="version">
-        <p>Hello, header!</p>
+    <div className="py-3 px-3 h-full flex flex-col items-center justify-center">
+      <div>
+        <p>Hello, main!</p>
+      </div>
+      <div>
+        <p>Hello, main!</p>
+      </div>
+      <div>
+        <p>Hello, main!</p>
       </div>
     </div>
   );
