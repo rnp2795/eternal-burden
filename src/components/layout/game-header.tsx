760c1151e5260bb27860c2dd4e0b765a5b0bbc1b
rnp2795 <death.pointer0t@icloud.com>
2024-09-21 13:58:40 +0300

    Add game pause state to header.

--- src/components/layout/game-header.tsx
+++ src/components/layout/game-header.tsx
@@ -1,11 +1,18 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { PauseToggle } from "../pause-toggle";
+import { useGameStateStore } from "@/lib/game-state-store";
 
 export const GameHeader = () => {
+  const { gameIsPaused } = useGameStateStore();
   return (
     <div className="border-b py-3 px-3 flex items-center justify-between">
       <div id="logo">
-        <p>Hello, header!</p>
+        <p>{import.meta.env.VITE_GAME_NAME}</p>
+      </div>
+      <div>
+        <p className="text-sm text-gray-400">
+          {gameIsPaused && "Game is paused."}
+        </p>
       </div>
       <div className="flex items-center gap-2">
         <ModeToggle />
