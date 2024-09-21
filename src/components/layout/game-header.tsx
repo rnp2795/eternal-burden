d52fd01635d17a09a40ba3da8d8c49e127a2bbc9
rnp2795 <death.pointer0t@icloud.com>
2024-09-21 13:07:01 +0300

    Add theme switch.

--- src/components/layout/game-header.tsx
+++ src/components/layout/game-header.tsx
@@ -1,9 +1,12 @@
+import { ModeToggle } from "@/components/mode-toggle";
+
 export const GameHeader = () => {
   return (
     <div className="border-b py-3 px-3 flex items-center justify-between">
       <div id="logo">
         <p>Hello, header!</p>
       </div>
+      <ModeToggle />
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { GameShell } from "./game-shell";
+import { ThemeProvider } from "./components/theme-provider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GameShell />
+    <ThemeProvider defaultTheme="dark" storageKey="game-ui-theme">
+      <GameShell />
+    </ThemeProvider>
   </StrictMode>
 );
