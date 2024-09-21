3ec8534c96ef3ca4574cae8449c73462e4888f41
rnp2795 <death.pointer0t@icloud.com>
2024-09-21 12:38:23 +0300

    Clean main.

--- src/components/layout/game-main.tsx
+++ src/components/layout/game-main.tsx
@@ -1,12 +1,6 @@
 export const GameMain = () => {
   return (
-    <div className="py-3 px-3 h-full flex flex-col items-center justify-center">
-      <div>
-        <p>Hello, main!</p>
-      </div>
-      <div>
-        <p>Hello, main!</p>
-      </div>
+    <div className="py-3 px-3 h-full flex flex-col items-center">
       <div>
         <p>Hello, main!</p>
       </div>
